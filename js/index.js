a1ebffa8636817a6b10348cf0b901e7842652f97
saad7528 <saad0174742@gmail.com>
2026-03-03 16:40:30 +0600

    add lessons and empty lesson section

--- js/index.js
+++ js/index.js
@@ -4,6 +4,50 @@ const loadData = () => {
     .then(json => display(json.data))
 }
 
+const loadLevelWord = (id) => {
+    const url = `https://openapi.programming-hero.com/api/level/${id}` 
+    fetch(url).then(res => res.json()).then(word => displayLoadLevelWord(word.data))
+}
+
+
+const displayLoadLevelWord = (words) =>{
+    const wordSection = document.getElementById("word-container")
+    wordSection.innerHTML = '';
+
+    if (words.length ==0){
+        wordSection.innerHTML = `
+        <div class="bg-gray-200 text-center items-center col-span-3 space-y-5 ">
+        <img class="mx-auto" src="./assets/alert-error.png" alt="">
+          <p class="text-gray-400">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+          <h1 class="text-3xl font-bold">নেক্সট Lesson এ যান।</h1>
+        </div>
+        `;
+
+    }
+
+    words.forEach(word => {
+        console.log(word);
+
+        const wordCart = document.createElement("div")
+        wordCart.innerHTML = `
+        <div class="bg-gray-50 py-10 px-5 rounded-lg shadow-sm space-y-6">
+          <h2 class="text-3xl/6 font-bold">${word.word}</h2>
+          <p class="text-sm/6 font-medium">Meaning /Pronounciation</p>
+          <div class=" text-3xl font-semibold font-bangla">"${word.meaning} / ${word.pronunciation}"</div>
+          <div class="flex justify-between ">
+            <button class="btn hover:bg-[#1A91FF20] p-2 rounded-sm"><i class=" text-gray-600 fa-solid fa-circle-info"></i></button>
+
+          <button class="btn hover:bg-[#1A91FF20] p-2 rounded-sm"><i class="text-gray-600 fa-solid fa-volume-high"></i></button>
+
+        </div>
+        `;
+
+        wordSection.append(wordCart);
+        
+    });
+
+
+}
 const display = (data) => {
 
     const levelContainer = document.getElementById("level-container")
@@ -13,7 +57,7 @@ const display = (data) => {
         console.log(item);
         const divLesson = document.createElement("div")
         divLesson.innerHTML = `
-        <button class="btn btn-outline btn-primary" ><i class="fa-solid fa-book-open"></i></i>Lesson - ${item.level_no}</button>
+        <button onclick="loadLevelWord(${item.level_no})" class="btn btn-outline btn-primary" ><i class="fa-solid fa-book-open"></i></i>Lesson - ${item.level_no}</button>
         
         `
         levelContainer.append(divLesson)
