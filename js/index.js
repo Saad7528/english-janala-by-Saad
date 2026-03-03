8663b2f2c53646a1d5b9e17269c3730549e9d6a4
saad7528 <saad0174742@gmail.com>
2026-03-03 18:11:03 +0600

    add button switching

--- js/index.js
+++ js/index.js
@@ -3,10 +3,22 @@ const loadData = () => {
     .then(rec => rec.json())
     .then(json => display(json.data))
 }
+// remove Lesson Btn css remove
+const removeActive = (id) => {
+const  lessonBtnCss = document.querySelectorAll(".lesson-btn")
+lessonBtnCss.forEach(btn => btn.classList.remove("active"))
+}
+
 
 const loadLevelWord = (id) => {
     const url = `https://openapi.programming-hero.com/api/level/${id}` 
-    fetch(url).then(res => res.json()).then(word => displayLoadLevelWord(word.data))
+    fetch(url)
+    .then(res => res.json())
+    .then(word => {
+        const lessonBtn = document.getElementById(`lesson-btn-${id}`)
+        removeActive();
+        lessonBtn.classList.add("active")
+        displayLoadLevelWord(word.data)})
 }
 
 
@@ -31,9 +43,9 @@ const displayLoadLevelWord = (words) =>{
         const wordCart = document.createElement("div")
         wordCart.innerHTML = `
         <div class="bg-gray-50 py-10 px-5 rounded-lg shadow-sm space-y-6">
-          <h2 class="text-3xl/6 font-bold">${word.word}</h2>
+          <h2 class="text-3xl/6 font-bold">${word.word? word.word : "শব্দ খুজে পাওয়া যায়নি"}</h2>
           <p class="text-sm/6 font-medium">Meaning /Pronounciation</p>
-          <div class=" text-3xl font-semibold font-bangla">"${word.meaning} / ${word.pronunciation}"</div>
+          <div class=" text-3xl font-semibold font-bangla">"${word.meaning? word.meaning : "অর্থ খুজে পাওয়া যায়নি"} / ${word.pronunciation? word.pronunciation : "উচ্চারণ খুজে পাওয়া যায়নি"}"</div>
           <div class="flex justify-between ">
             <button class="btn hover:bg-[#1A91FF20] p-2 rounded-sm"><i class=" text-gray-600 fa-solid fa-circle-info"></i></button>
 
@@ -57,7 +69,7 @@ const display = (data) => {
         console.log(item);
         const divLesson = document.createElement("div")
         divLesson.innerHTML = `
-        <button onclick="loadLevelWord(${item.level_no})" class="btn btn-outline btn-primary" ><i class="fa-solid fa-book-open"></i></i>Lesson - ${item.level_no}</button>
+        <button id="lesson-btn-${item.level_no}" onclick="loadLevelWord(${item.level_no})" class="lesson-btn btn btn-outline btn-primary" ><i class="fa-solid fa-book-open"></i></i>Lesson - ${item.level_no}</button>
         
         `
         levelContainer.append(divLesson)
